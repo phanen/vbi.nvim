acd5d5f3d4b7d6196cff1f2c56476b25be8ad53a
phanium <91544758+phanen@users.noreply.github.com>
2025-07-13 19:33:11 +0800

    init

---
+++ lua/vbi.lua
@@ -0,0 +1,60 @@
+local api, fn = vim.api, vim.fn
+local autocmd = api.nvim_create_autocmd
+local ns = api.nvim_create_namespace('u.vbi')
+local group = api.nvim_create_augroup('u.vbi', { clear = true })
+local auid
+
+local pos
+local update_pos = function() pos = fn.getcurpos() end
+local is_eol = function() return pos[5] == vim.v.maxcol end
+
+local attach = function()
+  local start_pos = fn.getpos("'<")
+  local end_pos = fn.getpos("'>")
+  local start_row, start_col, end_row, _ = start_pos[2], start_pos[3], end_pos[2] - 1, end_pos[3]
+  local cursor = api.nvim_win_get_cursor(0)
+  local origin_line = api.nvim_get_current_line()
+  local hl = (function()
+    local ctx = vim.inspect_pos()
+    return vim.tbl_get(ctx, 'semantic_tokens', 1, 'opts', 'hl_group_link')
+      or vim.tbl_get(ctx, 'treesitter', 1, 'hl_group_link')
+      or '@variable'
+  end)()
+  local marks = {}
+  if auid then api.nvim_del_autocmd(auid) end
+  auid = autocmd({ 'TextChangedI', 'CursorMovedI' }, {
+    group = group,
+    callback = function(ev)
+      local line = api.nvim_get_current_line()
+      -- TODO: diff? what's the actual behavior when feed <left>/<right>
+      local col = api.nvim_win_get_cursor(0)[2]
+      local text = line:sub(start_col, col)
+      local pos_type = is_eol() and 'eol' or 'inline'
+      for row = start_row, end_row do
+        local idx = row - start_row
+        local r = vim.F.npcall(api.nvim_buf_set_extmark, 0, ns, row, start_col - 1, {
+          id = marks[idx],
+          virt_text = { { text, hl } },
+          virt_text_pos = pos_type,
+        })
+        if r then
+          marks[idx] = r
+        else -- TODO: virtualedit=all?
+        end
+      end
+    end,
+  })
+end
+
+local detach = function()
+  if auid then
+    api.nvim_del_autocmd(auid)
+    auid = nil
+  end
+  api.nvim_buf_clear_namespace(0, ns, 0, -1)
+end
+
+autocmd('ModeChanged', { pattern = '\022:i', group = group, callback = attach })
+autocmd('InsertLeave', { pattern = '*', group = group, callback = detach })
+autocmd('CursorMoved', { pattern = '*', group = group, callback = update_pos })
+vim.schedule(update_pos)
