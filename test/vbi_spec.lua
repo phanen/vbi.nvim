1661971af8811e6bac9b95b7faa08ac65b112553
phanium <91544758+phanen@users.noreply.github.com>
2025-07-14 20:36:38 +0800

    test: init

---
+++ test/vbi_spec.lua
@@ -0,0 +1,127 @@
+---@diagnostic disable: invisible
+local n = require('nvim-test.helpers')
+local Screen = require('nvim-test.screen')
+local exec_lua = n.exec_lua
+
+describe('main', function()
+  local screen --- @type test.screen
+  before_each(function()
+    n.clear()
+    screen = Screen.new(30, 5)
+    screen:attach()
+    screen:set_default_attr_ids({
+      [1] = { background = Screen.colors.NvimLightYellow, foreground = Screen.colors.NvimDarkGrey1 },
+      [2] = { foreground = Screen.colors.NvimDarkGreen },
+    })
+    exec_lua(function() ---@diagnostic disable-next-line: duplicate-set-field
+      vim.opt.rtp:append('.')
+      vim.o.ve = 'block'
+      vim.o.sol = false -- this change `<c-q>G` behavior
+      vim.cmd.runtime { 'plugin/vbi.lua', bang = true }
+    end)
+  end)
+  it('chore', function()
+    n.api.nvim_buf_set_lines(0, 0, -1, false, {
+      'aaaaa',
+      'bbbbbbbbbbbb',
+      'cccccccccccccc',
+      'ddd',
+    })
+    n.feed('<c-q>GIabc')
+    screen:expect {
+      grid = [[
+        abc^aaaaa                      |
+        {1:abc}bbbbbbbbbbbb               |
+        {1:abc}cccccccccccccc             |
+        {1:abc}ddd                        |
+        {2:-- INSERT --}                  |
+      ]],
+    }
+    n.feed('<esc>')
+    screen:expect {
+      grid = [[
+        ^abcaaaaa                      |
+        abcbbbbbbbbbbbb               |
+        abccccccccccccccc             |
+        abcddd                        |
+                                      |
+      ]],
+    }
+
+    n.feed('gv2l<esc>dgv')
+    screen:expect {
+      grid = [[
+        ^aaaaa                         |
+        bbbbbbbbbbbb                  |
+        cccccccccccccc                |
+        ddd                           |
+                                      |
+      ]],
+    }
+
+    n.feed('<c-q>G$Axyz')
+    screen:expect {
+      grid = [[
+        aaaaaxyz^                      |
+        bbbbbbbbbbbb{1:xyz}               |
+        cccccccccccccc{1:xyz}             |
+        ddd{1:xyz}                        |
+        {2:-- INSERT --}                  |
+      ]],
+    }
+
+    n.feed('<esc>')
+    screen:expect {
+      grid = [[
+        ^aaaaaxyz                      |
+        bbbbbbbbbbbbxyz               |
+        ccccccccccccccxyz             |
+        dddxyz                        |
+                                      |
+      ]],
+    }
+
+    n.feed('gofy<c-q>GAA')
+    screen:expect {
+      grid = [[
+        aaaaaxyA^z                     |
+        bbbbbbb{1:A}bbbbbxyz              |
+        ccccccc{1:A}cccccccxyz            |
+        dddxyz{1: A}                      |
+        {2:-- INSERT --}                  |
+      ]],
+    }
+    n.feed('<esc>')
+    screen:expect {
+      grid = [[
+        aaaaax^yAz                     |
+        bbbbbbbAbbbbbxyz              |
+        cccccccAcccccccxyz            |
+        dddxyz A                      |
+                                      |
+      ]],
+    }
+
+    n.feed('gv3lo3lII')
+    screen:expect {
+      grid = [[
+        aaaaaxyAzI^                    |
+        bbbbbbbAb{1:I}bbbbxyz             |
+        cccccccAc{1:I}ccccccxyz           |
+        dddxyz A                      |
+        {2:-- INSERT --}                  |
+      ]],
+    }
+
+    n.feed('<esc>')
+    screen:expect {
+      grid = [[
+        aaaaaxyAz^I                    |
+        bbbbbbbAbIbbbbxyz             |
+        cccccccAcIccccccxyz           |
+        dddxyz A                      |
+                                      |
+      ]],
+    }
+  end)
+end)
